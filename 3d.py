8fbf34d2501c754ea2fe9ac33e4197017dc5962c
Yuannzh1 <165546950+Yuannzh1@users.noreply.github.com>
2024-05-02 16:25:43 +0800

    Update 3d.py

--- 3d.py
+++ 3d.py
@@ -1,13 +1,14 @@
 import plotly.graph_objects as go
 import numpy as np
-
+# Creating a grid of points in 3D space using np.mgrid
+# The grid spans from -2 to 2 in each dimension, with 100 points per dimension
 X, Y, Z = np.mgrid[-2:2:100j, -2:2:100j, -2:2:100j]
 
 degree=60
 a=1
 
 values =X**2+Y**2+Z**2+np.cos(degree*np.pi/180)*np.sqrt((X+a)**2+Y**2+Z**2)*np.sqrt((X-a)**2+Y**2+Z**2)-a**2
-
+# Creating a Plotly figure
 fig = go.Figure(data=go.Isosurface(
     x=X.flatten(),
     y=Y.flatten(),
